43ce03843fa3ff886daee353053009ac04bb2d34
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-05-22 13:13:16 +0300

    1task

---
+++ HW1.py
@@ -0,0 +1,18 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# ДЛЯ ТРЕХЗНАЧНОГО
+ 
+# n=int(input("Enter a three-digit number: "))
+# if n<100 or n>999:
+#     print("You've entered a non-three-digit number")
+# else:   
+#     print ("The sum of the digits in the three- digit number is:", n//100+n//10%10+n%10)
+
+# ДЛЯ ЛЮБОГО
+
+# n=int(input("Enter a number: ")) 
+# sum=0
+# while n > 0:
+#     digit = n%10
+#     sum= sum+digit
+#     n=n//10      
+# print("The sum of the digits in the number is:",sum)
